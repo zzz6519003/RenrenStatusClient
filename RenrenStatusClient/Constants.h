c8b8ac116d1d7f4ffd6fd0875135c225109e9628
zzz6519003 <zhengzhongzhao@gmail.com>
2014-05-07 23:28:08 +0800

    remove the SHITTTY SDK

--- RenrenStatusClient/Constants.h
+++ RenrenStatusClient/Constants.h
@@ -10,7 +10,7 @@
 #define RenrenStatusClient_Constants_h
 
 #define USER_STATUS_KEY @"user_status_key"
-//#define TOKEN @"195797|6.7b133b7b564b487c9e3265615e196951.2592000.1401361200-279838227"
+#define TOKEN @"195797|6.7b133b7b564b487c9e3265615e196951.2592000.1401361200-279838227"
 #define TOKEN [[NSUserDefaults standardUserDefaults] objectForKey:TOKEN_KEY]
 #define TOKEN_KEY @"accessToken"
 #define STATUS_ARRAY @"status_array_key"
--- RenrenStatusClient/README.md
+++ RenrenStatusClient/README.md
@@ -1,2 +1,3 @@
 ###
 a silly client for Renren to view friend's status
+
